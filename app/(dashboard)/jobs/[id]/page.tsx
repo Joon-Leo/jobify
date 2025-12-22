69a81438bb4def6c83beba7a7113a5e8e62da211
Joonsenia <128702580+Joon-Leo@users.noreply.github.com>
2025-12-22 23:27:12 +0900

    feat:update job page

---
+++ app/(dashboard)/jobs/[id]/page.tsx
@@ -0,0 +1,25 @@
+import EditJobForm from "@/components/EditJobForm";
+import { getSingleJobAction } from "@/utils/actions";
+
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+
+async function JobDetailPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["job", id],
+    queryFn: () => getSingleJobAction(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <EditJobForm jobId={id} />
+    </HydrationBoundary>
+  );
+}
+export default JobDetailPage;
